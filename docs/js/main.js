057ae4447ab27504ce309770e197e79533a6bd1c
danmtt <37252746+danmtt@users.noreply.github.com>
2020-07-30 00:14:48 +0100

    Update JS (jQuery) + clear code

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,8 +1,31 @@
-function onClickEvent(){
-    var quadrants= document.getElementsByClassName("quadrant");
-    // console.log(quadrants);
-    var i;
-    for (i = 0; i < quadrants.length; i++) {
-    quadrants[i].style.display = 'block';
-      }
-    }
+// function onClickNavMenuEvent(){  
+//   var quadrants= document.getElementsByClassName("quadrant");
+//     console.log(quadrants);
+//     var i;
+//     for (i = 0; i < quadrants.length; i++) {
+//       quadrants[i].style.display="block";
+//       quadrants[i].classList.add("quadrant_transform");
+//     }
+//   };
+
+  // function onClickCentralLogo(){
+  //   var quadrants= document.getElementsByClassName("quadrant");
+  //   // console.log(quadrants);
+  //   var i;
+  //   for (i = 0; i < quadrants.length; i++) {
+  //   quadrants[i].style.display = "none";
+  //     }
+  //   };
+
+// jQuery CODE AREA
+// ---------------------------------------------------------------------
+$(document).ready(
+  function(){
+  $("#nav_wrap").click(function(){
+    $(".quadrant").fadeIn(750);
+       });
+  
+  $(".central_logo").click(function(){
+    $(".quadrant").fadeOut(750);
+  });
+});
