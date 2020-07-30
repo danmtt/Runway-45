7d12de34329a01107c33766530cac40a8f5c068f
danmtt <37252746+danmtt@users.noreply.github.com>
2020-07-30 23:46:20 +0100

    Update jQuery/CSS  and HTML clean and refactor

--- docs/js/main.js
+++ docs/js/main.js
@@ -17,15 +17,63 @@
   //     }
   //   };
 
+  // function rotateSpanText(){
+  //   var quadrants= document.getElementsByClassName("quadrant");
+
+  // };
+  
+  //   var quadrants= document.getElementsByClassName("quadrant");
+  //   // console.log(quadrants);
+  //   var i;
+  //   for (i = 0; i < quadrants.length; i++) {
+  //   quadrants[i].style.display = "none";
+  //     }
+  //   };
+
 // jQuery CODE AREA
 // ---------------------------------------------------------------------
 $(document).ready(
   function(){
   $("#nav_wrap").click(function(){
-    $(".quadrant").fadeIn(750);
-       });
+    $("#q01").fadeIn(1650);
+    $("#q02").fadeIn(450);
+    $("#q03").fadeIn(850);
+    $("#q04").fadeIn(1250);
+    $(".central_logo").fadeIn(550);
+  });
   
   $(".central_logo").click(function(){
-    $(".quadrant").fadeOut(750);
+    $("#q01").fadeOut(450);
+    $("#q02").fadeOut(1050);
+    $("#q03").fadeOut(850);
+    $("#q04").fadeOut(650);
+    $(".central_logo").fadeOut(1250);
+    $("#qinfo").fadeOut(1250);
   });
+// fadeOut on non clicked quadrants
+  $("#q01").click(function(){
+    $("#q02").fadeOut(650);
+    $("#q03").fadeOut(650);
+    $("#q04").fadeOut(650);
+    $("#qinfo").fadeIn(650);
+  });
+  $("#q02").click(function(){
+    $("#q01").fadeOut(650);
+    $("#q03").fadeOut(650);
+    $("#q04").fadeOut(650);
+    $("#qinfo").fadeIn(650);
+  });
+  $("#q03").click(function(){
+    $("#q01").fadeOut(650);
+    $("#q02").fadeOut(650);
+    $("#q04").fadeOut(650);
+    $("#qinfo").fadeIn(650);
+  });
+  $("#q04").click(function(){
+    $("#q01").fadeOut(650);
+    $("#q02").fadeOut(650);
+    $("#q03").fadeOut(650);
+    $("#qinfo").fadeIn(650);
+  });
+
 });
