9fc2571bf468978c20959d9a99b12120692b88bf
danmtt <37252746+danmtt@users.noreply.github.com>
2020-07-31 23:35:38 +0100

    Update jQuery: Display Functions+HTML/CSS id upgrade

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,56 +1,38 @@
-// function onClickNavMenuEvent(){  
-//   var quadrants= document.getElementsByClassName("quadrant");
-//     console.log(quadrants);
-//     var i;
-//     for (i = 0; i < quadrants.length; i++) {
-//       quadrants[i].style.display="block";
-//       quadrants[i].classList.add("quadrant_transform");
-//     }
-//   };
-
-  // function onClickCentralLogo(){
-  //   var quadrants= document.getElementsByClassName("quadrant");
-  //   // console.log(quadrants);
-  //   var i;
-  //   for (i = 0; i < quadrants.length; i++) {
-  //   quadrants[i].style.display = "none";
-  //     }
-  //   };
-
-  // function rotateSpanText(){
-  //   var quadrants= document.getElementsByClassName("quadrant");
-
-  // };
-  
-  //   var quadrants= document.getElementsByClassName("quadrant");
-  //   // console.log(quadrants);
-  //   var i;
-  //   for (i = 0; i < quadrants.length; i++) {
-  //   quadrants[i].style.display = "none";
-  //     }
-  //   };
-
 // jQuery CODE AREA
 // ---------------------------------------------------------------------
 $(document).ready(
   function(){
+
+  // fadeIn quadrants when clicking on fake nav menu
+  // Also displaying quadrant_text (span elements)
   $("#nav_wrap").click(function(){
-    $("#q01").fadeIn(1650);
-    $("#q02").fadeIn(450);
+    $("#q01").fadeIn(1650);    
+    $("#q02").fadeIn(450);    
     $("#q03").fadeIn(850);
-    $("#q04").fadeIn(1250);
-    $(".central_logo").fadeIn(550);
+    $("#q04").fadeIn(1250);    
+    $("#sp01").fadeIn(1650);
+    $("#sp02").fadeIn(450);
+    $("#sp03").fadeIn(850);
+    $("#sp04").fadeIn(1250);
+    $("#central_logo").fadeIn(550);
   });
-  
-  $(".central_logo").click(function(){
+  // fadeOut quadrants when clicking on central logo
+  // Also hiding quadrant text, quadrant info and this (central logo)
+  $("#central_logo").click(function(){
     $("#q01").fadeOut(450);
     $("#q02").fadeOut(1050);
     $("#q03").fadeOut(850);
     $("#q04").fadeOut(650);
-    $(".central_logo").fadeOut(1250);
+
+    $("#sp01").fadeOut(450);
+    $("#sp02").fadeOut(1050);
+    $("#sp03").fadeOut(850);
+    $("#sp04").fadeOut(650);
+    $(this).fadeOut(1250);
     $("#qinfo").fadeOut(1250);
   });
-// fadeOut on non clicked quadrants
+// fadeOut non clicked quadrants when clicking individually
+// Also displaying quadrant info
   $("#q01").click(function(){
     $("#q02").fadeOut(650);
     $("#q03").fadeOut(650);
