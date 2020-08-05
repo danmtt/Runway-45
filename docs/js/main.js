29cd4e4fe44b624568aa7c5575d05df3cbd05990
Danmtt <danielmartintrujillo@gmail.com>
2020-08-05 07:10:45 +0100

    Update jS: qinfo link optimizations

--- docs/js/main.js
+++ docs/js/main.js
@@ -44,6 +44,7 @@ $(document).ready(
     // Flex call out to organize quadrant info box when displayed
     $('#qinfo').css('display', 'flex');
     $("#qinfo").fadeIn(650);
+    document.getElementById("qinfofooter").innerHTML ="";
     var q01 = document.getElementById("sp01").innerHTML;
     document.getElementById("qinfoheader").innerHTML = q01;
     var q01_link = document.createElement('a');
@@ -56,24 +57,48 @@ $(document).ready(
     $("#q03").fadeOut(650);
     $("#q04").fadeOut(650);
     $("#qinfo").fadeIn(650);
+    // Flex call out to organize quadrant info box when displayed
+    $('#qinfo').css('display', 'flex');
+    $("#qinfo").fadeIn(650);
+    document.getElementById("qinfofooter").innerHTML = "";
     var q02 = document.getElementById("sp02").innerHTML;
     document.getElementById("qinfoheader").innerHTML = q02;
+    var q02_link = document.createElement('a');
+    q02_link.href = "";
+    q02_link.innerText = "Go to " + q02;
+    document.getElementById("qinfofooter").appendChild(q02_link);
   });
   $("#q03").click(function(){
     $("#q01").fadeOut(650);
     $("#q02").fadeOut(650);
     $("#q04").fadeOut(650);
     $("#qinfo").fadeIn(650);
+    // Flex call out to organize quadrant info box when displayed
+    $('#qinfo').css('display', 'flex');
+    $("#qinfo").fadeIn(650);
+    document.getElementById("qinfofooter").innerHTML = "";
     var q03 = document.getElementById("sp03").innerHTML;
     document.getElementById("qinfoheader").innerHTML = q03;
+    var q03_link = document.createElement('a');
+    q03_link.href = "";
+    q03_link.innerText = "Go to " + q03;
+    document.getElementById("qinfofooter").appendChild(q03_link);
   });
   $("#q04").click(function(){
     $("#q01").fadeOut(650);
     $("#q02").fadeOut(650);
     $("#q03").fadeOut(650);
     $("#qinfo").fadeIn(650);
+    // Flex call out to organize quadrant info box when displayed
+    $('#qinfo').css('display', 'flex');
+    $("#qinfo").fadeIn(650);
+    document.getElementById("qinfofooter").innerHTML = "";
     var q04 = document.getElementById("sp04").innerHTML;
     document.getElementById("qinfoheader").innerHTML = q04;
+    var q04_link = document.createElement('a');
+    q04_link.href = "";
+    q04_link.innerText = "Go to " + q04;
+    document.getElementById("qinfofooter").appendChild(q04_link);
   });
 
 });
