2cf0e6efb5ce46ca1e47c2815f241834c0629d70
danmtt <37252746+danmtt@users.noreply.github.com>
2020-08-08 22:14:44 +0100

    Update onclick events behaviours

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,6 +1,12 @@
 $(document).ready(
   function(){
 
+  // Var definitions
+  // -------------------------------------  
+  var element_clicked;
+
+  // Functions
+  // -------------------------------------  
   // Function grouping jQuery calls to fadeIn all quadrants, texts and central logo image. 
   function quadrantsFadeIn(){
     $("#q01").fadeIn(1650);    
@@ -16,7 +22,7 @@ $(document).ready(
     $("#sp03").fadeIn(850);
     $("#sp04").fadeIn(1250);
     $("#central_logo").fadeIn(550);
-    };
+  };
   // Function grouping jQuery calls to fadeOut quadrants, texts, quadrant info box and central_logo image. 
   function quadrantsFadeOut(){
     $("#q01").fadeOut(450);
@@ -26,73 +32,63 @@ $(document).ready(
     $("#sp01").fadeOut(450);
     $("#sp02").fadeOut(1050);
     $("#sp03").fadeOut(850);
-    $("#sp04").fadeOut(650);
-    $("#central_logo").fadeOut(1250);
-   // $("#qinfo").fadeOut(1250);
+    $("#sp04").fadeOut(650); 
   };
+  // Function grouping jQuery calls to fadeIn quadrant information box. 
   function quadrantInfoFadeIn(){
     $("#qinfo").fadeIn(650);
     // Flex call out to organize quadrant info box when displayed
     $("#qinfo").css("display", "flex");
   };
+  // Function to identify element from quadrant and export to quadrant info box elements.
+  function exportQuadrantElement(){    
+    var quadrant_name = document.getElementById(element_clicked).innerHTML;
+    document.getElementById("qinfo_title").innerHTML = quadrant_name;
+    document.getElementById("qinfo_link").innerHTML = quadrant_name;
+  };
   
-  
+  // jQuery on click events
+  // -------------------------------------  
   // jQuery targetting fake nav menu on click event.   
   $("#nav_wrap").click(function(){
     quadrantsFadeIn();
-    $("#qinfo").fadeOut(1250);
   });
   // jQuery targetting central logo on click event.
   $("#central_logo").click(function(){
     quadrantsFadeOut();
+    $("#central_logo").fadeOut(1250);
     $("#qinfo").fadeOut(1250);
   });
-  // jQuery functions (4) on click events targetting individual elements one at a time:
-  // call out to fadeOut unnecesary elements not clicked 
-  // + call out to fadeIn #qinfo(quadrants info box) with header optimization. 
+  // jQuery functions (4) targetting individual elements on click events
+  // to fadeOut unnecesary elements and to quadrant info box optimized. 
   $("#q01").click(function(){
+    element_clicked = $(this).attr("id");
     quadrantsFadeOut();
     quadrantInfoFadeIn();
-    var q01 = document.getElementById("sp01").innerHTML;
-    document.getElementById("qinfo_title").innerHTML = q01;
-    document.getElementById("qinfo_link").innerHTML ="";
-    var q01_link = document.createElement("a");
-    q01_link.href = "";
-    q01_link.innerText = q01;
-    document.getElementById("qinfo_link").appendChild(q01_link);
+    exportQuadrantElement();
   });
   $("#q02").click(function(){
+    element_clicked = $(this).attr("id");
     quadrantsFadeOut();
     quadrantInfoFadeIn();
-    var q02 = document.getElementById("sp02").innerHTML;
-    document.getElementById("qinfo_title").innerHTML = q02;
-    document.getElementById("qinfo_link").innerHTML ="";
-    var q02_link = document.createElement("a");
-    q02_link.href = "";
-    q02_link.innerText = q02;
-    document.getElementById("qinfo_link").appendChild(q02_link);
+    exportQuadrantElement();
   });
   $("#q03").click(function(){
+    element_clicked = $(this).attr("id");
     quadrantsFadeOut();
     quadrantInfoFadeIn();
-    var q03 = document.getElementById("sp03").innerHTML;
-    document.getElementById("qinfo_title").innerHTML = q03;
-    document.getElementById("qinfo_link").innerHTML ="";
-    var q03_link = document.createElement("a");
-    q03_link.href = "";
-    q03_link.innerText = q03;
-    document.getElementById("qinfo_link").appendChild(q03_link);
+    exportQuadrantElement();
   });
   $("#q04").click(function(){
+    element_clicked = $(this).attr("id");
     quadrantsFadeOut();
     quadrantInfoFadeIn();
-    var q04 = document.getElementById("sp04").innerHTML;
-    document.getElementById("qinfo_title").innerHTML = q04;
-    document.getElementById("qinfo_link").innerHTML ="";
-    var q04_link = document.createElement("a");
-    q04_link.href = "";
-    q04_link.innerText = q04;
-    document.getElementById("qinfo_link").appendChild(q04_link);
+    exportQuadrantElement();
+  });
+  $("#qinfo_home").click(function(){
+    element_clicked = $(this).attr("id");
+    $("#qinfo").fadeOut(1250);
+    quadrantsFadeIn(); 
   });
 
 });
